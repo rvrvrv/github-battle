4cfd6e70f5f7b009e870fbc9348bbf2e5e962332
rvrvrv <ibanez19@sbcglobal.net>
2017-02-11 09:19:41 -0600

    Add Axios - GitHub API call

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,14 @@
 var React = require('react');
 
+
+function showIt(obj) {
+	return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+}
+
 function ConfirmBattle(props) {
 	return props.isLoading === true
 		? <p>Loading!</p>
-		: <p>Confirm Battle!</p>
+		: <div>Confirm Battle!: {showIt(props)}</div>
 }
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,8 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
+
+
 var ConfirmBattleContainer = React.createClass({
 	contextTypes: {
 		router: React.PropTypes.object.isRequired
@@ -12,8 +15,14 @@ var ConfirmBattleContainer = React.createClass({
 	},
 	componentDidMount: function() {
 		var query = this.props.location.query; //PlayerOne and PlayerTwo
-		//Fetch info from GitHub, then update state
-		
+		//Get both players' info from Github API call (via our utils)
+		githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+			.then(function(players) {
+				this.setState({
+					isLoading: false,
+					playerInfo: [players[0], players[1]]
+				});
+			}.bind(this));
 	},
 	render: function() {
 		return (
@@ -22,7 +31,6 @@ var ConfirmBattleContainer = React.createClass({
 			playerInfo={this.state.playerInfo} />
 		);
 	}
-	
 });
 
 module.exports = ConfirmBattleContainer;
